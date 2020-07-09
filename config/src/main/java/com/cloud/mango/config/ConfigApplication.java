97bce8487900a0497e0a0f262a3d1e48d16c004a
赵帅 <353080647@qq.com>
2020-07-09 16:02:24 +0800

    spring cloud config  @mango

---
+++ config/src/main/java/com/cloud/mango/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.cloud.mango.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心
+ *
+ * @author mango
+ * @date 2020/7/9
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
---
+++ config/src/test/java/com/cloud/mango/config/ConfigApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cloud.mango.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
